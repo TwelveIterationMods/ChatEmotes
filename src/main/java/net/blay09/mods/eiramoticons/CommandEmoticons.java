20c4300f9e7f71aee7cf4e93e0fbcc1e90ee08ad
Blay09 <blay09@hotmail.de>
2015-06-03 22:21:16 +0200

    Add /emoticons config command.
    Fixes #25

--- src/main/java/net/blay09/mods/eiramoticons/CommandEmoticons.java
+++ src/main/java/net/blay09/mods/eiramoticons/CommandEmoticons.java
@@ -6,6 +6,8 @@ package net.blay09.mods.eiramoticons;
 import net.blay09.mods.eiramoticons.emoticon.EmoticonGroup;
 import net.blay09.mods.eiramoticons.emoticon.EmoticonHandler;
 import net.blay09.mods.eiramoticons.emoticon.EmoticonRegistry;
+import net.blay09.mods.eiramoticons.gui.GuiEiraMoticonsConfig;
+import net.minecraft.client.Minecraft;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.command.WrongUsageException;
@@ -14,6 +16,9 @@ import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.ChatComponentTranslation;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.IChatComponent;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CommandEmoticons extends CommandBase {
 
@@ -24,7 +29,7 @@ public class CommandEmoticons extends CommandBase {
 
 	@Override
 	public String getCommandUsage(ICommandSender sender) {
-		return "/emoticons reload";
+		return "/emoticons reload|help|list|config";
 	}
 
 	@Override
@@ -48,12 +53,26 @@ public class CommandEmoticons extends CommandBase {
 			linkComponent.getChatStyle().setUnderlined(true);
 			sender.addChatMessage(new ChatComponentTranslation("eiramoticons:command.help", linkComponent));
 		} else if(args[0].equals("list")) {
-			for(EmoticonGroup group : EmoticonRegistry.getGroups()) {
+			for (EmoticonGroup group : EmoticonRegistry.getGroups()) {
 				sender.addChatMessage(group.listComponent);
 			}
+		} else if(args[0].equals("config")) {
+			Minecraft.getMinecraft().displayGuiScreen(new GuiEiraMoticonsConfig(null));
 		} else {
 			throw new WrongUsageException(getCommandUsage(sender));
 		}
 	}
 
+	@Override
+	public List addTabCompletionOptions(ICommandSender sender, String[] args) {
+		if(args.length == 0) {
+			List<String> list = new ArrayList<String>();
+			list.add("reload");
+			list.add("help");
+			list.add("list");
+			list.add("config");
+			return list;
+		}
+		return null;
+	}
 }
